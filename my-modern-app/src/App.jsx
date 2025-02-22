71f3b342d339aadefa90937cbddc9a9a6ab7cc2e
Netra Joshi <joshi241@purdue.edu>
2025-02-22 17:59:53 -0500

    added more buttons on website, will edit soon

--- my-modern-app/src/App.jsx
+++ my-modern-app/src/App.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
-import Leaderboard from './components/Leaderboard'
-import Login from './components/Login'
-import Account from './components/Account'
-import './App.css'
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import Leaderboard from './components/Leaderboard';
+import Login from './components/Login';
+import Account from './components/Account';
+import './App.css';
+import './styles/Navbar.css';
 
 function App() {
-    const { isLoading, isAuthenticated, error } = useAuth0()
+    const { isLoading, isAuthenticated, error } = useAuth0();
 
     if (isLoading) {
-        return <div>Loading...</div>
+        return <div>Loading...</div>;
     }
 
     if (error) {
-        return <div>Oops... {error.message}</div>
+        return <div>Oops... {error.message}</div>;
     }
 
     return (
         <div className="main-container">
+            <div className="navbar">
+                <div className="navbar-brand">
+                    <h2>My App</h2>
+                </div>
+                <div className="navbar-buttons">
+                    <button className="navbar-button" onClick={() => window.location.href = '/home'}>Home</button>
+                    <button className="navbar-button" onClick={() => window.location.href = '/your-tab-management-stats'}>Analytics Dashboard</button>
+                </div>
+            </div>
             <div className="header">
                 {isAuthenticated ? (
                     <Account />
@@ -29,7 +39,7 @@ function App() {
                 <Leaderboard />
             </div>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
